68f5d068a5e82b3d52e318396cc6b366c1d293d4
Davide <Davide@host.docker.internal>
2024-07-29 18:33:25 +0200

    update filterchain

--- src/main/java/it/social/configuration/WebSecurityConfig.java
+++ src/main/java/it/social/configuration/WebSecurityConfig.java
@@ -21,50 +21,45 @@ import it.social.security.jwt.AuthEntryPointJwt;
 import it.social.security.jwt.AuthTokenFilter;
 import jakarta.servlet.http.HttpServletResponse;
 
-
 @Configuration
 @EnableMethodSecurity
-//(securedEnabled = true,
-//jsr250Enabled = true,
-//prePostEnabled = true) // by default
 public class WebSecurityConfig {
   
-  @Value("${spring.h2.console.path}")
-  private String h2ConsolePath;
+    @Value("${spring.h2.console.path}")
+    private String h2ConsolePath;
   
-  @Value("${social.app.frontendUrl}")
-  private String frontendUrl;
+    @Value("${social.app.frontendUrl}")
+    private String frontendUrl;
   
-  @Autowired
-  UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
 
-  @Autowired
-  private AuthEntryPointJwt unauthorizedHandler;
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
 
     @Bean
     AuthTokenFilter authenticationJwtTokenFilter() {
-    return new AuthTokenFilter();
-  }
+        return new AuthTokenFilter();
+    }
 
     @Bean
     DaoAuthenticationProvider authenticationProvider() {
-      DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-       
-      authProvider.setUserDetailsService(userDetailsService);
-      authProvider.setPasswordEncoder(passwordEncoder());
-   
-      return authProvider;
-  }
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
 
     @Bean
     AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-    return authConfig.getAuthenticationManager();
-  }
+        return authConfig.getAuthenticationManager();
+    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors(cors -> cors.configurationSource(request -> {
@@ -88,58 +83,10 @@ public class WebSecurityConfig {
             .requestMatchers("/context-path/**").permitAll()
             .anyRequest().authenticated()
         )
-        .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint((request, response, authException) -> {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.setContentType("application/json");
-            response.getWriter().write("{\"message\": \"Error: You need to be authenticated to access this resource.\"}");
-        }))
         .headers(headers -> headers.frameOptions(frameOption -> frameOption.sameOrigin()))
         .authenticationProvider(authenticationProvider())
         .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
-//    @Bean
-//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//    http.cors(cors -> cors.disable())
-//    	.csrf(csrf -> csrf.disable())
-//        .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
-//        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//        .authorizeHttpRequests(auth -> 
-//        	auth.requestMatchers("/").permitAll()
-//        	  .requestMatchers("/api/signup/**").permitAll()
-//              .requestMatchers("/api/signin/**").permitAll()
-//              .requestMatchers(h2ConsolePath + "/**").permitAll()
-//              .requestMatchers("/swagger-ui/**").permitAll()
-//              .requestMatchers("/v3/api-docs/**").permitAll()
-//              .requestMatchers("/context-path/**").permitAll()
-//              .anyRequest().authenticated()
-//        )
-//        .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint((request, response, authException) -> {
-//            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-//            response.setContentType("application/json");
-//            response.getWriter().write("{\"message\": \"Error: You need to be authenticated to access this resource	.\"}");
-//            }));
-//    
-// // fix H2 database console: Refused to display ' in a frame because it set 'X-Frame-Options' to 'deny'
-//    http.headers(headers -> headers.frameOptions(frameOption -> frameOption.sameOrigin()));
-//    
-//    http.authenticationProvider(authenticationProvider());
-//
-//    http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-//    
-//    return http.build();
-//  }
-//    
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/**").allowedOrigins("http://localhost:3000") // Your frontend URL
-//						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*")
-//						.allowCredentials(true); // Allow credentials (cookies, authorization headers, etc.)
-//			}
-//		};
-//	}
 }
