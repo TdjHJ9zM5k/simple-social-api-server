da4c25160bbaa37ca65db72437aef08aff7968a6
Davide <Davide@host.docker.internal>
2024-07-30 00:07:10 +0200

    samesite fix attempt 2

--- src/main/java/it/social/configuration/CookieConfig.java
+++ src/main/java/it/social/configuration/CookieConfig.java
@@ -1,18 +1,18 @@
-package it.social.configuration;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.session.web.http.DefaultCookieSerializer;
-import org.springframework.session.web.http.CookieSerializer;
-
-@Configuration
-public class CookieConfig {
-
-    @Bean
-    public CookieSerializer cookieSerializer() {
-        DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer();
-        cookieSerializer.setSameSite("None");
-        cookieSerializer.setUseSecureCookie(true); // Ensure this is true in production to send cookies over HTTPS
-        return cookieSerializer;
-    }
-}
+//package it.social.configuration;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.session.web.http.DefaultCookieSerializer;
+//import org.springframework.session.web.http.CookieSerializer;
+//
+//@Configuration
+//public class CookieConfig {
+//
+//    @Bean
+//    public CookieSerializer cookieSerializer() {
+//        DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer();
+//        cookieSerializer.setSameSite("None");
+//        cookieSerializer.setUseSecureCookie(true); // Ensure this is true in production to send cookies over HTTPS
+//        return cookieSerializer;
+//    }
+//}
--- src/main/java/it/social/configuration/WebSecurityConfig.java
+++ src/main/java/it/social/configuration/WebSecurityConfig.java
@@ -14,17 +14,21 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import it.social.security.UserDetailsServiceImpl;
 import it.social.security.jwt.AuthEntryPointJwt;
 import it.social.security.jwt.AuthTokenFilter;
 import jakarta.servlet.http.HttpServletResponse;
 
+
 @Configuration
 @EnableMethodSecurity
+//(securedEnabled = true,
+//jsr250Enabled = true,
+//prePostEnabled = true) // by default
 public class WebSecurityConfig {
   
   @Value("${spring.h2.console.path}")
@@ -40,44 +44,55 @@ public class WebSecurityConfig {
   private AuthEntryPointJwt unauthorizedHandler;
 
   @Bean
-  AuthTokenFilter authenticationJwtTokenFilter() {
+    AuthTokenFilter authenticationJwtTokenFilter() {
     return new AuthTokenFilter();
   }
 
   @Bean
-  DaoAuthenticationProvider authenticationProvider() {
-    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-    authProvider.setUserDetailsService(userDetailsService);
-    authProvider.setPasswordEncoder(passwordEncoder());
-    return authProvider;
+    DaoAuthenticationProvider authenticationProvider() {
+      DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+       
+      authProvider.setUserDetailsService(userDetailsService);
+      authProvider.setPasswordEncoder(passwordEncoder());
+   
+      return authProvider;
   }
 
   @Bean
-  AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
     return authConfig.getAuthenticationManager();
   }
 
   @Bean
-  PasswordEncoder passwordEncoder() {
+    PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+    
+  
+  @Bean
+  public CookieSerializer cookieSerializer() {
+      DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+      serializer.setSameSite("None");
+      serializer.setUseSecureCookie(true);
+      return serializer;
   }
 
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.cors(cors -> cors.configurationSource(request -> {
-      CorsConfiguration corsConfig = new CorsConfiguration();
-      corsConfig.addAllowedOrigin("http://localhost:3000");
-      corsConfig.addAllowedOrigin("https://social-network-frontend-cpkhh4qfda-ew.a.run.app");
-      corsConfig.addAllowedMethod("*");
-      corsConfig.addAllowedHeader("*");
-      corsConfig.setAllowCredentials(true);
-      return corsConfig;
-    }))
-    .csrf(csrf -> csrf.disable())
-    .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
-    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-    .authorizeHttpRequests(auth ->
-      auth.requestMatchers("/").permitAll()
+      http.cors(cors -> cors.configurationSource(request -> {
+          CorsConfiguration corsConfig = new CorsConfiguration();
+          corsConfig.addAllowedOrigin("http://localhost:3000");
+          corsConfig.addAllowedOrigin("https://social-network-frontend-cpkhh4qfda-ew.a.run.app");
+          corsConfig.addAllowedMethod("*");
+          corsConfig.addAllowedHeader("*");
+          corsConfig.setAllowCredentials(true);
+          return corsConfig;
+      }))
+      .csrf(csrf -> csrf.disable())
+      .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+      .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+      .authorizeHttpRequests(auth ->
+          auth.requestMatchers("/").permitAll()
           .requestMatchers("/api/signup/**").permitAll()
           .requestMatchers("/api/signin/**").permitAll()
           .requestMatchers(h2ConsolePath + "/**").permitAll()
@@ -85,29 +100,59 @@ public class WebSecurityConfig {
           .requestMatchers("/v3/api-docs/**").permitAll()
           .requestMatchers("/context-path/**").permitAll()
           .anyRequest().authenticated()
-    )
-    .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint((request, response, authException) -> {
-      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-      response.setContentType("application/json");
-      response.getWriter().write("{\"message\": \"Error: You need to be authenticated to access this resource.\"}");
-    }))
-    .headers(headers -> headers.frameOptions(frameOption -> frameOption.sameOrigin()))
-    .authenticationProvider(authenticationProvider())
-    .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+      )
+      .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint((request, response, authException) -> {
+          response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+          response.setContentType("application/json");
+          response.getWriter().write("{\"message\": \"Error: You need to be authenticated to access this resource.\"}");
+      }))
+      .headers(headers -> headers.frameOptions(frameOption -> frameOption.sameOrigin()))
+      .authenticationProvider(authenticationProvider())
+      .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
-    return http.build();
+      return http.build();
   }
-
-  @Bean
-  public CorsFilter corsFilter() {
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    CorsConfiguration config = new CorsConfiguration();
-    config.setAllowCredentials(true);
-    config.addAllowedOrigin("http://localhost:3000");
-    config.addAllowedOrigin("https://social-network-frontend-cpkhh4qfda-ew.a.run.app");
-    config.addAllowedHeader("*");
-    config.addAllowedMethod("*");
-    source.registerCorsConfiguration("/**", config);
-    return new CorsFilter(source);
-  }
+//    @Bean
+//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//    http.cors(cors -> cors.disable())
+//    	.csrf(csrf -> csrf.disable())
+//        .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+//        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//        .authorizeHttpRequests(auth -> 
+//        	auth.requestMatchers("/").permitAll()
+//        	  .requestMatchers("/api/signup/**").permitAll()
+//              .requestMatchers("/api/signin/**").permitAll()
+//              .requestMatchers(h2ConsolePath + "/**").permitAll()
+//              .requestMatchers("/swagger-ui/**").permitAll()
+//              .requestMatchers("/v3/api-docs/**").permitAll()
+//              .requestMatchers("/context-path/**").permitAll()
+//              .anyRequest().authenticated()
+//        )
+//        .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint((request, response, authException) -> {
+//            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//            response.setContentType("application/json");
+//            response.getWriter().write("{\"message\": \"Error: You need to be authenticated to access this resource	.\"}");
+//            }));
+//    
+// // fix H2 database console: Refused to display ' in a frame because it set 'X-Frame-Options' to 'deny'
+//    http.headers(headers -> headers.frameOptions(frameOption -> frameOption.sameOrigin()));
+//    
+//    http.authenticationProvider(authenticationProvider());
+//
+//    http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+//    
+//    return http.build();
+//  }
+//    
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**").allowedOrigins("http://localhost:3000") // Your frontend URL
+//						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*")
+//						.allowCredentials(true); // Allow credentials (cookies, authorization headers, etc.)
+//			}
+//		};
+//	}
 }
